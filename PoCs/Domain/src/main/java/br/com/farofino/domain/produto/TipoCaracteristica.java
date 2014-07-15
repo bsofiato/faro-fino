3aac0a9158d978fa4b53b9458ee2826dc66a42ae
bruno <bruno.sofiato@gmail.com>
2014-07-15 01:39:16 -0300

    Tipo de Caracteristica

--- PoCs/Domain/src/main/java/br/com/farofino/domain/produto/TipoCaracteristica.java
+++ PoCs/Domain/src/main/java/br/com/farofino/domain/produto/TipoCaracteristica.java
@@ -1,11 +1,14 @@
 package br.com.farofino.domain.produto;
 
+import java.io.IOException;
 import java.io.Serializable;
+import org.codehaus.jackson.map.ObjectMapper;
 
-public class TipoCaracteristica implements Serializable {
+public class TipoCaracteristica <Type> implements Serializable {
+  
   private String id;
   private String nome;
-  private Class type;
+  private Class <Type> type;
   
   private boolean defining;
 
@@ -33,11 +36,25 @@ public class TipoCaracteristica implements Serializable {
     this.defining = defining;
   }
 
-  public Class getType() {
+  public Class <Type> getType() {
     return type;
   }
 
-  public void setType(Class type) {
+  public void setType(Class <Type> type) {
     this.type = type;
+  }
+  
+  public String marshall(Type object) throws IOException {
+    if (object != null) {
+      return TipoCaracteristicaMarshallerServiceLocator.getInstance().getMapper().writeValueAsString(object);
+    }
+    return null;
+  }
+  
+  public Type unmarshall(String value) throws IOException {
+    if (value != null) {
+      return TipoCaracteristicaMarshallerServiceLocator.getInstance().getMapper().readValue(value, getType());
+    }
+    return null;
   }
 }
